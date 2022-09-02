bf1b00c234232a9d1cbec4f69af582048e0491a6
Rafael <rafael_breno@outlook.com>
2022-09-01 23:17:50 -0300

    feat: Add sample reader

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/rafaelbreno/maze/maze"
+)
+
+const (
+	sampleFolder = "samples/"
+)
+
+func main() {
+	samples, err := ioutil.ReadDir(sampleFolder)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, sample := range samples {
+		b, err := os.ReadFile(fmt.Sprintf("%s%s", sampleFolder, sample.Name()))
+		if err != nil {
+			panic(err)
+		}
+
+		m := maze.Maze{}
+
+		err = json.Unmarshal(b, &m)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(maze.GetExitPath(m))
+	}
+}
