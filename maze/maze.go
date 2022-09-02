86c8fb3dc0b871197b3660f7e581b6e62b8c0c43
Rafael <rafael_breno@outlook.com>
2022-09-01 21:40:00 -0300

    feat: Add PrettyPrinter

--- maze/maze.go
+++ maze/maze.go
@@ -1,5 +1,21 @@
 package maze
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Maze is the data struct representing
 // a given maze.
 type Maze map[string]any
+
+// PrettyPrint - prints a indented JSON
+// representation of Maze.
+func (m *Maze) PrettyPrint() {
+	b, err := json.MarshalIndent(*m, "", "	")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(b))
+}
